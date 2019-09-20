1fd8a9f0bee31a3710bd0d8c435d3a1df5c7895b
Shkell2 <darkromik@mail.ru>
2019-09-20 17:20:42 +0300

    6.3. Массив заполнен true или false[#175989]

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -8,17 +8,17 @@ public class TurnTest {
     @Test
     public void test1() {
         Turn turn = new Turn();
-        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] input = new int[] {1, 2, 3, 4, 5, 6};
         int[] res = turn.back(input);
-        int[] expected = new int[] {5, 4, 3, 2, 1};
+        int[] expected = new int[] {6, 5, 4, 3, 2, 1};
         assertThat(res, is(expected));
     }
     @Test
     public void test2() {
         Turn turn = new Turn();
-        int[] input = new int[] {1, 4, 3, 8};
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         int[] res = turn.back(input);
-        int[] expected = new int[] {8, 3, 4, 1};
+        int[] expected = new int[] {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         assertThat(res, is(expected));
     }
 }
